5ab354542514ffd151789eb40ea3d82762a71be1
kimonvl <kimonvloutis@gmail.com>
2025-04-15 16:33:18 +0300

    first commit

---
+++ src/main/java/com/example/security_tutorial/SecurityTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/security_tutorial/SecurityTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
